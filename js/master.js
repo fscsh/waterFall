c0b9b203726db4ef1dd6d13a80c631b8ef67a639
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-09-30 22:02:06 -0700

    addWaterfall

--- js/master.js
+++ js/master.js
@@ -1,11 +1,56 @@
 $(document).ready(function() {
     $(window).on("load", function() {
         imglocation();
+        var dataimg = {
+            "data": [{
+                "src": "1.jpg"
+            }, {
+                "src": "2.jpg"
+            }, {
+                "src": "3.png"
+            }, {
+                "src": "4.jpg"
+            }, {
+                "src": "5.jpg"
+            }, {
+                "src": "6.jpg"
+            }, {
+                "src": "7.jpg"
+            }, {
+                "src": "8.png"
+            }, {
+                "src": "9.png"
+            }, {
+                "src": "10.jpg"
+            }, {
+                "src": "11.jpg"
+            }]
+        }
+        window.onscroll = function() {
+            if (getheight()) {
+                $.each(dataimg.data, function(index, value) {
+                    var box = $("<div>").addClass("box").appendTo($("#container"));
+                    var content = $("<div>").addClass("content").appendTo(box);
+                    // console.log("img/" + $(value).attr("src"));
+                    $("<img>").attr("src", "img/" + $(value).attr("src")).appendTo(content);
+
+                });
+                imglocation();
+            }
+        };
     });
 });
 
+function getheight() {
+    var box = $(".box");
+    var totalhtight = box.last().get(0).offsetTop + Math.floor(box.last().height()) / 2;
+    var documentheight = $(document).width();
+    var scrollheight = $(window).scrollTop();
+    return (totalhtight < documentheight + scrollheight) ? true : false;
+}
+
+
 function imglocation() {
-    // var box = $(".box");
     var boxwidth = $(".box").eq(0).width();
     var num = Math.floor($(window).width() / boxwidth)
     var boxarr = [];
@@ -21,7 +66,7 @@ function imglocation() {
                 "top": minboxheight,
                 "left": $(".box").eq(minboxindex).position().left
             })
-            boxarr[minboxindex]+=$(".box").eq(index).height();
+            boxarr[minboxindex] += $(".box").eq(index).height();
         }
 
     });
